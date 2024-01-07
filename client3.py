071cb5c28827f8fe2ca5c64f93b1342f0a5b9d76
Sofie Christensen <sofiec510@gmail.com>
2024-01-07 17:35:38 -0500

    complete getDataPoint tests, write getRatio test

--- client3.py
+++ client3.py
@@ -44,6 +44,7 @@ def getRatio(price_a, price_b):
     """ ------------- Update this function ------------- """
     if (price_b == 0):
         return
+    # will return None ^
     return price_a/price_b
 
 
--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +18,20 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
 
+  def test_getRatio_calculateRatio(self):
+      price_data = [ 
+        {'price_a': 16, 'price_b': 8, 'expected_ratio': 2},
+        {'price_a': 120, 'price_b': 40, 'expected_ratio': 3},
+        {'price_a': 100, 'price_b': 0, 'expected_ratio': None}
+      ]
+      for args in price_data:
+        self.assertEqual(getRatio(args['price_a'], args['price_b']), args['expected_ratio'])
 
 
 if __name__ == '__main__':
-    unittest.main()
+      unittest.main()
